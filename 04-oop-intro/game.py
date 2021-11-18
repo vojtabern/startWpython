bcf7926297dc1b1c80b15e98a40cde9739b6f98d
vojtabern <vojtabern@gmail.com>
2021-11-18 11:18:42 +0100

    game_podruhe

--- 04-oop-intro/game.py
+++ 04-oop-intro/game.py
@@ -1,8 +1,15 @@
 import datetime
+from random import randrange
+from enum import Enum
+
+
+class Sex(Enum):
+    male = "man"
+    female = "woman"
 
 
 class Person:
-    def __init__(self, nickname: str, sex: str):
+    def __init__(self, nickname: str, sex: Sex):
         self.nickname = nickname
         self.sex = sex
         self.__birth = datetime.datetime.now()
@@ -14,9 +21,19 @@ class Person:
         actual_time = datetime.datetime.now()
         return int((actual_time - self.__birth).total_seconds())
 
+    @property
+    def sex(self):
+        return self.__sex
+
+    @sex.setter
+    def sex(self, sex):
+        if sex in Sex:
+            self.__sex = sex
+        else:
+            raise ValueError("Sex Value is not valid")
 
 class Player(Person):
-    def __init__(self, nickname: str, sex: str, state: str):
+    def __init__(self, nickname: str, sex: Sex, state: str):
         super().__init__(nickname, sex)
         self.state = state
         self.count_of_games = 0
@@ -35,13 +52,60 @@ class Player(Person):
             return float("{:.2f}".format(rate))
 
 
+class Match:
+    def __init__(self, house_player: Player, guest_player: Player):
+        self.h_player = house_player
+        self.g_player = guest_player
+        self.__datetime = datetime.datetime.now()
+        self.hp_points = 0
+        self.gp_points = 0
+        self.__history = []
+
+    def __str__(self):
+        return f"{self.h_player.nickname} vs. {self.g_player.nickname} {self.hp_points}:{self.gp_points}"
+
+    def __roll(self):
+        while True:
+            hp = randrange(1, 6)
+            gp = randrange(1,6)
+            if hp!=gp:
+                break
+        return 0 if hp > gp else 1
+
+    def play(self):
+        while self.hp_points < 10 and self.gp_points < 10:
+            if self.__roll() == 0:
+                self.hp_points += 1
+            else:
+                self.gp_points += 1
+            self.__history.append(self.score())
+        self.h_player.score["plus"] += self.hp_points
+        self.h_player.score["minus"] += self.gp_points
+
+        self.g_player.score["plus"] += self.gp_points
+        self.g_player.score["minus"] += self.hp_points
+
+        if self.hp_points > self.gp_points:
+            self.h_player.wins += 1
+        else:
+            self.g_player.wins += 1
+
+    def score(self):
+        return self.hp_points, self.gp_points
+
+    def get_history(self):
+        return self.__history
 # test
 
-person = Person("Karel", "man")
-player = Player("Jana", "woman", "CZE")
-player.count_of_games = 555
-player.wins = 308
+
+player1 = Player("Karel", Sex("man"), "CZE")
+player2 = Player("Jana", Sex("woman"), "CZE")
+# player.count_of_games = 555
+# player.wins = 308
 input("Pockej chvili a zadej enter")
-print(person, person.get_seconds_from_birth())
-print(player, player.get_seconds_from_birth())
-print(f"{player.win_rate()}%")
+# print(person, person.get_seconds_from_birth())
+# print(player, player.get_seconds_from_birth())
+# print(f"{player.win_rate()}%")
+match = Match(player1, player2)
+match.play()
+print(str(match), f"\nHistorie: {match.get_history()}")
